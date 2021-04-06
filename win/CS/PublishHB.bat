15929319530be9c78335b83838e013a2fbff726b
Synapto <bruce@darkfactor.xyz>
2021-04-06 17:42:02 +1000

    Why am I compiling for OTHER people's systems?

--- win/CS/PublishHB.bat
+++ win/CS/PublishHB.bat
@@ -1,19 +1,17 @@
 @REM BUG: Dittums DotNet doesn't like any files existing in the destination directory
 Del /F /Q .\HandBrakeWPF\bin\publish\*.*
 
-@REM Build the non-shared library version of HB
+@REM Build the static library version of HB (single .EXE)
 DotNet publish -p:PublishProfile=publish_x64
 
 @REM Remove redundant files
 Del /F /Q .\HandBrakeWPF\bin\publish\*.pdb
 
-@REM Copy required support files into destination directory
+@REM Copy required support files into the destination directory
 XCopy ..\..\build\HandBrakeCLI.exe .\HandBrakeWPF\bin\publish /V /C /R /Y
 XCopy ..\..\build\libhb\hb.dll     .\HandBrakeWPF\bin\publish /V /C /R /Y
-
 XCopy .\HandBrake.Worker\bin\publish\*.exe .\HandBrakeWPF\bin\publish /I /V /C /R /Y
-
 XCopy .\doc\*.* .\HandBrakeWPF\bin\publish\doc /I /V /C /R /Y
 
-@REM Visual feedback that it worked
+@REM Visual feedback that it all worked
 Dir /O:GN .\HandBrakeWPF\bin\publish
